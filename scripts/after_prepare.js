d9cb81ad3045775d3d7bc64e1330706a850b4f6e
Quentin Loriot <QuentinLORIOT@users.noreply.github.com>
2021-04-07 12:31:45 +0200

    Update after_prepare.js
    update pathes for electron

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -16,8 +16,8 @@ var config = fs.readFileSync("config.xml").toString()
 var APP_ID = getPreferenceValue(config, "intercom-app-id")
 
 var files = [
-    "platforms/browser/www/plugins/cordova-plugin-intercom/src/electron/intercom-electron.js",
-    "platforms/browser/platform_www/plugins/cordova-plugin-intercom/src/electron/intercom-electron"
+    "platforms/electron/www/plugins/cordova-plugin-intercom/src/electron/intercom-electron.js",
+    "platforms/electron/platform_www/plugins/cordova-plugin-intercom/src/electron/intercom-electron"
 ]
 
 for(var i in files) {
